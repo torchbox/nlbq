34ee1db8b809ff904c038f8a27aca5da1802f082
zerolab <dan@zerolab.org>
2023-06-01 12:23:29 +0100

    Add support for generating prompt from BigQuery table

--- nlbq/cli.py
+++ nlbq/cli.py
@@ -3,12 +3,14 @@ import sys
 from asyncio import run
 from functools import wraps
 from pathlib import Path
+from typing import Annotated
 
 import typer
 from tabulate import tabulate
 
 from nlbq.api import serve as api_serve
 from nlbq.core import DEFAULT_MODEL, NLBQ
+from nlbq.helpers import generate_prompt_from_dataset_table
 
 
 class AsyncTyper(typer.Typer):
@@ -50,20 +52,55 @@ async def ask(query: str, model: str = DEFAULT_MODEL) -> str:
     print("\n" + tabulate(rows, headers=fields, tablefmt="pipe") + "\n")
 
 
+def validate_table_format(table_in_dataset: str) -> str:
+    """Validate dataset format"""
+    if "." not in table_in_dataset:
+        raise typer.BadParameter(
+            "The target table value must be in the format dataset.table_id"
+        )
+    return table_in_dataset
+
+
 @app.command()
-def init():
+def init(
+    table_in_dataset: Annotated[
+        str,
+        typer.Option(
+            "--table",
+            "-t",
+            help="The target table in the form of dataset.table_id",
+            callback=validate_table_format,
+        ),
+    ] = None
+):
     """Create stub files: prompt.txt, index.html, Dockerfile"""
     package_dir = Path(__file__).parent
-    for file in ["Dockerfile", "index.html", "prompt.txt"]:
-        dest = Path.cwd() / file
+    current_dir = Path.cwd()
+    prefix_success = typer.style("✔", fg=typer.colors.GREEN, bold=True)
+    prefix_error = typer.style("✘", fg=typer.colors.RED, bold=True)
+
+    for file in ["Dockerfile", "index.html"]:
+        dest = current_dir / file
         if dest.exists():
-            prefix = typer.style("✘", fg=typer.colors.RED, bold=True)
-            typer.echo(f"{prefix} {file} already exists here")
+            typer.echo(f"{prefix_error} {file} already exists here")
             raise typer.Abort()
         else:
             shutil.copyfile(package_dir / "templates" / file, dest)
-            prefix = typer.style("✔", fg=typer.colors.GREEN, bold=True)
-            typer.echo(f"{prefix} Created {file} at {dest}")
+            typer.echo(f"{prefix_success} Created {file} at {dest}")
+
+    prompt_file = current_dir / "prompt.txt"
+    if prompt_file.exists():
+        typer.echo(f"{prefix_error} {file} already exists here")
+        raise typer.Abort()
+
+    if table_in_dataset:
+        shutil.copyfile(package_dir / "templates" / "prompt_template.txt", prompt_file)
+        print(f"⠹ Creating prompt.txt using {table_in_dataset}", end="\r")
+        generate_prompt_from_dataset_table(table_in_dataset, prompt_file, NLBQ())
+    else:
+        shutil.copyfile(package_dir / "templates" / "prompt.txt", prompt_file)
+
+    typer.echo(f"{prefix_success} Created prompt.txt at {prompt_file}")
 
 
 @app.command()
---
+++ nlbq/helpers.py
@@ -0,0 +1,47 @@
+import json
+from datetime import date, datetime
+from pathlib import Path
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from nlbq.core import NLBQ
+
+
+class CustomJsonEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, datetime | date):
+            return o.isoformat()
+        return super().default(o)
+
+
+def generate_prompt_from_dataset_table(
+    table_in_dataset: str, prompt_file: Path, nlbq: "NLBQ"
+):
+    """Generate a prompt file from a dataset and table"""
+    dataset_id, table_id = table_in_dataset.split(".")
+
+    fields = {
+        field.name: str(field)
+        for field in nlbq.client.get_table(table_in_dataset).schema
+    }
+
+    column_description_glue = ": add column description here\n"
+    content = prompt_file.read_text()
+    updated_content = (
+        content.replace("{{ project_id }}", nlbq.client.project)
+        .replace("{{ dataset }}", dataset_id)
+        .replace("{{ table_id }}", table_id)
+        .replace("{{ schema }}", "\n".join(fields.values()))
+        .replace(
+            "{{ columns }}",
+            column_description_glue.join(fields.keys()) + column_description_glue,
+        )
+    )
+
+    preview_rows = list(nlbq.client.list_rows(table_in_dataset, max_results=3))
+    preview = json.dumps(
+        [dict(row) for row in preview_rows], indent=2, cls=CustomJsonEncoder
+    )
+    updated_content = updated_content.replace("{{ preview }}", preview)
+
+    prompt_file.write_text(updated_content)
